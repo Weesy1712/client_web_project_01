41c775ba48ac511441577d0ad57e7de06cca0840
Karim Tayari <mr.moki111@gmail.com>
2021-01-29 16:29:53 +0100

    Added rest of the files. Also added Sample code. Navbar included

---
+++ libs/javascript/myScript.js
@@ -0,0 +1,65 @@
+function moreFunction1() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("myBtn1");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "MEHR";
+        moreText.style.display = "none";
+
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "WENIGER";
+        moreText.style.display = "inline";
+    }
+}
+
+function moreFunction2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("myBtn2");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "MEHR";
+        moreText.style.display = "none";
+
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "WENIGER";
+        moreText.style.display = "inline";
+    }
+}
+function moreFunction3() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("myBtn3");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "MEHR";
+        moreText.style.display = "none";
+
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "WENIGER";
+        moreText.style.display = "inline";
+    }
+}
+function moreFunction4() {
+    var dots = document.getElementById("dots4");
+    var moreText = document.getElementById("more4");
+    var btnText = document.getElementById("myBtn4");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "MEHR";
+        moreText.style.display = "none";
+
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "WENIGER";
+        moreText.style.display = "inline";
+    }
+}
